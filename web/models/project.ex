7b109a0152ee242334c031dc08911b82aa00c27d
Andrew Kappen <akappen@gmail.com>
2016-11-06 12:47:43 -0600

    Project gets rating and custom validation

--- web/models/project.ex
+++ web/models/project.ex
@@ -4,6 +4,7 @@ defmodule PhoenixApp.Project do
   schema "projects" do
     field :name, :string
     field :slug, :string
+    field :rating, :float, virtual: true
     has_many :lists, PhoenixApp.List
     many_to_many :users, PhoenixApp.User, join_through: "projects_users"
     has_many :items, through: [:lists, :items]
@@ -19,5 +20,15 @@ defmodule PhoenixApp.Project do
     |> cast(params, [:name, :slug])
     |> validate_required([:name, :slug])
     |> unique_constraint(:slug)
+  end
+
+  def validate_slug_contains_name(changeset) do
+    slug = get_field(changeset, :slug)
+    name = get_field(changeset, :name)
+    if Regex.match?(~r[#{name}]i, slug) do
+      changeset
+    else
+      add_error(changeset, :slug, "must contain '#{name}'")
+    end
   end
 end
